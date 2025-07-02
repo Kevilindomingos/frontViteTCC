f999dcbba27451a31a5e4b38d09a2369ea3573b8
Kevilin Marcondes <kevilinmarcondes356@gmail.com>
2025-07-01 22:18:28 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import style from './App.module.css'
+import { api } from './api/api'
+import { useNavigate } from 'react-router'
+import { useState, useEffect } from 'react';
+
+function App() {
+  
+  return (
+    <>
+    </>
+  )
+}
+
+export default App
