c09afc7715a2f0fc7b4299b2438eba5c2e86929c
Kevilin Marcondes <kevilinmarcondes356@gmail.com>
2025-07-16 07:44:06 -0300

    Add files via upload

--- src/AboutUs.jsx
+++ src/AboutUs.jsx
@@ -1,3 +1,4 @@
+/*  */
 import { useNavigate } from "react-router";
 import { useEffect, useState } from "react";
 import { Menu } from "./components/menu";
--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
+/* página inicial de login */
+
 import style from './App.module.css'
 import { api } from './api/api'
 import { useNavigate } from 'react-router'
 import { useState, useEffect } from 'react';
+import { ChevronDown, ChevronUp, User, Check, UserRound } from 'lucide-react';
+import { select } from './components/select'
 
 function App() {
   const navigate = useNavigate();
 
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+  const [type, setType] = useState('')
   const [user, setUser] = useState(null)
   const [message, setMessage] = useState('')
   const [showPassword, setShowPassword] = useState(false)
@@ -23,7 +28,7 @@ function App() {
   const handleLogin = async (e) => {
     e.preventDefault()
     try {
-      const response = await api.post('/login', { email, password })
+      const response = await api.post('/login', { email, password, type})
       const user = response.data
 
       localStorage.setItem('user', JSON.stringify(user))
@@ -43,18 +48,49 @@ function App() {
     <div className={style.wrapForm}>
       <form onSubmit={handleLogin}>
         <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center'}}>
-        {/* <img className={style.iconLogin} src={IconLogin} alt="Icone arrow" /> */}
         <h2>Login</h2>
         </div>
         <div style={{position: "relative", width: "100%"}}>
-          {/* <img className={style.icon} src={Icon} alt="Icone arrow" /> */}
           <input type="email" placeholder='Email' value={email} onChange={(e) => setEmail(e.target.value)} required />
         </div>
         <div style={{position: "relative", width: "100%"}}>
-          {/* <img className={style.icon} src={Icon} alt="Icone arrow" /> */}
           <input type={showPassword ? 'text' : 'password'} placeholder='Senha' value={password} onChange={(e) => setPassword(e.target.value)} required />
-          {/* <img onClick={() => setShowPassword(prev => !prev)} style={{position: "absolute", width: '20px', borderRadius: '100%', right: '10px', top: '10px', cursor: 'pointer'}} src={Eye} alt="Olho da senha" /> */}
         </div>
+
+        <div className={style.select}>
+          <div className={style.categorySelect}>
+            <label htmlFor="options-view-button">Categoria</label>
+            <input type="checkbox" className={style.optionsViewButton}/>
+
+            <div className={style.selectButton}>
+              <div className={style.selectedValue}>Selecione a categoria</div>
+
+              <div className={style.chevrons}>
+                <ChevronDown className={style.ChevronDown}/>
+                <ChevronUp className={style.ChevronUp}/>
+              </div>
+            </div>
+          </div>
+
+          <ul className={style.options}>
+            <li className={style.option}>
+              <input type="radio" placeholder='Responsável' value={type} onChange={(e) => setType(e.target.value)} required />
+              <User className={style.responsavel}/>
+              <span className={style.label}>Responsável</span>
+              <Check className={style.checkIcon}/>
+            </li>
+          </ul>
+
+          <ul className={style.options}>
+            <li className={style.option}>
+              <input type="radio" placeholder='Funcionário' value={type} onChange={(e) => setType(e.target.value)} required />
+              <UserRound className={style.responsavel}/>
+              <span className={style.label}>Funcionário</span>
+              <Check className={style.checkIcon}/>
+            </li>
+          </ul>
+        </div>
+
         <button type='submit'>Entrar</button>
         <p>{message}</p>
       </form>
@@ -65,3 +101,7 @@ function App() {
 }
 
 export default App
+
+{/* <div style={{position: "relative", width: "100%"}}>
+          <input type="type" placeholder='Type' value={type} onChange={(e) => setType(e.target.value)} required />
+        </div> */}
