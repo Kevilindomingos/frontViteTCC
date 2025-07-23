ec3efda20576ecd4989cfc10e9168a51f31cf833
Kevilin Marcondes <kevilinmarcondes356@gmail.com>
2025-07-23 10:20:21 -0300

    Add files via upload

--- src/DailyList.jsx
+++ src/DailyList.jsx
@@ -1,4 +1,4 @@
-import './dailyList.module.css'
+import style from './dailyList.module.css'
 import { useEffect, useState } from 'react'
 import { api } from './api/api'
 import { Menu } from './components/menu'
@@ -143,176 +143,51 @@ function DailyList() {
 
   return (
     <>
-      <section>
+      <section className={style.fundo}>
         <Menu />
+        
         <div style={{ padding: '2rem' }}>
-          <h1>Rotinas dos Idosos</h1>
-
-          {/* CRIAÇÃO */}
-          <h2>Criar Nova Rotina</h2>
-          <form
-            onSubmit={handleNewSubmit}
-            style={{
-              display: 'flex',
-              flexDirection: 'column',
-              gap: '0.5rem',
-              maxWidth: '400px'
-            }}
-          >
-            <label>
-              Idoso:
-              <select
-                value={selectedIdosoId}
-                onChange={(e) => setSelectedIdosoId(e.target.value)}
-                required
-              >
+          <h1 className={style.titulo}>Rotinas dos Idosos</h1>
+{/* CRIAÇÃO */}
+          <form className={style.formulario} onSubmit={handleNewSubmit} style={{ display: 'flex', flexDirection: 'column', gap: '0.5rem', maxWidth: '400px' }} >
+            <h2 className={style.tituloForm}>Criar Nova Rotina</h2>
+            <label> Idoso: <select value={selectedIdosoId} onChange={(e) => setSelectedIdosoId(e.target.value)} required >
                 <option value="">Selecione um idoso</option>
                 {idosos.map((idoso) => (
-  <option key={idoso.id} value={idoso.id}>
-    {`${idoso.id} - ${idoso.name}`}  {/* mudar aqui de nome para name */}
-  </option>
-))}
+                  <option key={idoso.id} value={idoso.id}> {`${idoso.id} - ${idoso.name}`} </option>
+                ))}
               </select>
             </label>
-            <input
-              type="text"
-              name="horaRefeicao"
-              value={newData.horaRefeicao}
-              onChange={handleNewChange}
-              placeholder="Hora da refeição"
-              required
-            />
-            <input
-              type="text"
-              name="medicamentos"
-              value={newData.medicamentos}
-              onChange={handleNewChange}
-              placeholder="Medicamentos"
-              required
-            />
-            <input
-              type="text"
-              name="atvRealizadas"
-              value={newData.atvRealizadas}
-              onChange={handleNewChange}
-              placeholder="Atividades realizadas"
-              required
-            />
-            <input
-              type="text"
-              name="humorGeral"
-              value={newData.humorGeral}
-              onChange={handleNewChange}
-              placeholder="Humor geral"
-              required
-            />
-            <input
-              type="text"
-              name="higienePessoal"
-              value={newData.higienePessoal}
-              onChange={handleNewChange}
-              placeholder="Higiene pessoal"
-              required
-            />
-            <button type="submit">Criar rotina</button>
+            <input type="text" name="horaRefeicao" value={newData.horaRefeicao} onChange={handleNewChange} placeholder="Hora da refeição" required />
+            <input type="text" name="medicamentos" value={newData.medicamentos} onChange={handleNewChange} placeholder="Medicamentos" required />
+            <input type="text" name="atvRealizadas" value={newData.atvRealizadas} onChange={handleNewChange} placeholder="Atividades realizadas" required />
+            <input type="text"  name="humorGeral" value={newData.humorGeral} onChange={handleNewChange} placeholder="Humor geral" required />
+            <input type="text" name="higienePessoal" value={newData.higienePessoal} onChange={handleNewChange} placeholder="Higiene pessoal" required />
+            <button className={style.botao} type="submit">Criar rotina</button>
           </form>
-
           {/* SELECIONAR */}
-          <h2 style={{ marginTop: '2rem' }}>Selecionar rotina para editar ou visualizar</h2>
+          <h2 className={style.titulo} style={{ marginTop: '2rem' }}>Selecionar rotina para editar ou visualizar</h2>
           <label>
-            <select
-              onChange={handleSelectChange}
-              value={selectedListId}
-              style={{ marginTop: '0.5rem' }}
-            >
+            <select className={style.selectRotina} onChange={handleSelectChange} value={selectedListId} style={{ marginTop: '0.5rem' }}>
               <option value="">Selecione...</option>
               {lists.map((list) => (
-                <option key={list.id} value={list.id}>
-                  {`Rotina ${list.id} - ${list.horaRefeicao}`}
-                </option>
+                <option key={list.id} value={list.id}> {`Rotina ${list.id} - ${list.horaRefeicao}`} </option>
               ))}
             </select>
           </label>
 
           {/* EDIÇÃO */}
           {editListId && (
-            <form
-              onSubmit={handleUpdate}
-              style={{
-                display: 'flex',
-                flexDirection: 'column',
-                gap: '0.5rem',
-                marginTop: '1rem',
-                maxWidth: '400px'
-              }}
-            >
-              <input
-                type="text"
-                name="horaRefeicao"
-                value={editData.horaRefeicao}
-                onChange={handleEditChange}
-                required
-              />
-              <input
-                type="text"
-                name="medicamentos"
-                value={editData.medicamentos}
-                onChange={handleEditChange}
-                required
-              />
-              <input
-                type="text"
-                name="atvRealizadas"
-                value={editData.atvRealizadas}
-                onChange={handleEditChange}
-                required
-              />
-              <input
-                type="text"
-                name="humorGeral"
-                value={editData.humorGeral}
-                onChange={handleEditChange}
-                required
-              />
-              <input
-                type="text"
-                name="higienePessoal"
-                value={editData.higienePessoal}
-                onChange={handleEditChange}
-                required
-              />
+            <form className={style.visualizacao} onSubmit={handleUpdate} style={{ display: 'flex', flexDirection: 'column', gap: '0.5rem', marginTop: '1rem', maxWidth: '400px'}}>
+              <input type="text" name="horaRefeicao" value={editData.horaRefeicao} onChange={handleEditChange} required />
+              <input type="text" name="medicamentos" value={editData.medicamentos} onChange={handleEditChange} required />
+              <input type="text" name="atvRealizadas" value={editData.atvRealizadas} onChange={handleEditChange} required />
+              <input type="text" name="humorGeral" value={editData.humorGeral} onChange={handleEditChange} required />
+              <input type="text" name="higienePessoal" value={editData.higienePessoal} onChange={handleEditChange} required />
               <button type="submit">Salvar alterações</button>
-              <button
-                type="button"
-                onClick={() => {
-                  setEditListId(null)
-                  setSelectedListId('')
-                }}
-              >
-                Cancelar
-              </button>
+              <button type="button" onClick={() => { setEditListId(null)
+                  setSelectedListId('') }}> Cancelar </button>
             </form>
-          )}
-
-          {/* VISUALIZAR APENAS A ROTINA SELECIONADA */}
-          {selectedListId && (
-            <div style={{ marginTop: '2rem' }}>
-              {lists
-                .filter((list) => list.id === parseInt(selectedListId))
-                .map((list) => (
-                  <div key={list.id}>
-                    <p><strong>Hora da Refeição:</strong> {list.horaRefeicao}</p>
-                    <p><strong>Medicamentos:</strong> {list.medicamentos}</p>
-                    <p><strong>Atividades Realizadas:</strong> {list.atvRealizadas}</p>
-                    <p><strong>Humor Geral:</strong> {list.humorGeral}</p>
-                    <p><strong>Higiene Pessoal:</strong> {list.higienePessoal}</p>
-                    <div style={{ display: 'inline-flex', gap: '0.5rem', marginTop: '0.5rem' }}>
-                      <button onClick={() => handleSelectChange({ target: { value: list.id } })}>Editar</button>
-                      <button onClick={() => handleDelete(list.id)}>Deletar</button>
-                    </div>
-                  </div>
-                ))}
-            </div>
           )}
         </div>
       </section>
@@ -321,3 +196,4 @@ function DailyList() {
 }
 
 export default DailyList
+
